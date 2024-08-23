2926532a05566e116a95bd0b0e4e0c845d317180
geonamics2020 <geonamics@gmail.com>
2024-08-23 23:30:05 +0300

    Update server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -34,7 +34,7 @@ Handler buildRootHandler() {
 Handler buildHandler() {
   final pipeline = const Pipeline().addMiddleware(logRequests());
   final router = Router()
-    ..get('/', Response.ok('Hello, World!',))
+    ..get('/', rootHandler)
     ..get('/new', newHandler)
     ..get('/add/<id>/', playerLandingHandler) //_addPlayerHandler
     ..post('/add/<id>/submitName', nameHandler) // ?name=<name>&answer=<answer>
